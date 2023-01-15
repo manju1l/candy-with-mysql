584315cb97760df4c0d41b6e7b7c2193421a54fd
python <110155631+manju1l@users.noreply.github.com>
2023-01-15 19:40:38 +0530

    Add files via upload

---
+++ exam/src/main/java/com/example/exam/model/Model.java
@@ -0,0 +1,30 @@
+package com.example.exam.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Model {
+	@Id
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Model [id=" + id + ", name=" + name + "]";
+	}
+	
+
+}
